426de711ed375871cb0c32bece4587dbf37c12dd
HirokiUmatani <o9093009555@gmail.com>
2015-08-09 22:58:22 +0900

    refact container view in top view controller

---
+++ library/BaseContainerviewController.h
@@ -0,0 +1,13 @@
+//
+//  BaseContainerviewController.h
+//  LessonApp
+//
+//  Created by hirokiumatani on 2015/08/09.
+//  Copyright (c) 2015年 hirokiumatani. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface BaseContainerviewController : UIViewController
+
+@end
---
+++ library/BaseContainerviewController.m
@@ -0,0 +1,37 @@
+//
+//  BaseContainerviewController.m
+//  LessonApp
+//
+//  Created by hirokiumatani on 2015/08/09.
+//  Copyright (c) 2015年 hirokiumatani. All rights reserved.
+//
+
+#import "BaseContainerviewController.h"
+
+@interface BaseContainerviewController ()
+
+@end
+
+@implementation BaseContainerviewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
